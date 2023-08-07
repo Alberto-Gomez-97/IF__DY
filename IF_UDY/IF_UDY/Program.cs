ba7e3d946f079e4976969418a30c8d9399724b61
Alberto-Gomez-97 <120439839+Alberto-Gomez-97@users.noreply.github.com>
2023-08-06 18:33:35 -0600

    Add files via upload

---
+++ IF_UDY/IF_UDY/Program.cs
@@ -0,0 +1,47 @@
+﻿var CANT1 = 4;
+
+if (CANT1 > 5)
+{
+    Console.WriteLine("ES MAYOR A 5");
+}
+else if (CANT1 == 3) {
+    Console.WriteLine("El valor es 3!!");
+    }
+else
+{
+    Console.WriteLine("Es menor o igual a 5");
+}
+Console.WriteLine("FIN");
+
+/* EJERCICIO CURSO 46 IF
+ * namespace DecisionesyBucles.Ejercicios
+{
+    
+    Utiliza "if-else" para determinar cuál entero es más grande, a o b. No tienes que declarar estas variables, pues ya esto está hecho.
+
+    Debes asignar el valor más grande a la variable resultado.
+
+    Tú código debe ir debajo del comentario y encima del return.
+
+    Remueve las dos líneas indicadas más abajo para descomentar el código y comenzar el ejercicio.
+     
+
+public class NumeroMayor
+    {
+        public int DeterminarElNumeroMayor(int a, int b)
+        {
+            int resultado;
+
+            if (a > b)
+            {
+                resultado = a;
+            }
+            else (a < b)
+             {
+                resultado = b;
+            }
+
+            return resultado;
+        }
+}
+} */
